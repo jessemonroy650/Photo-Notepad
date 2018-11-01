b3780a113fee5d9e927260da7793543d5552a476
jessemonroy650 <jessemonroy650@yahoo.com>
2018-11-01 01:44:16 -0700

    initial entry

---
+++ photonotepad.js
@@ -0,0 +1,18 @@
+//
+// Photo Notepad
+//	Date: 2018-11-01
+//
+var appConfig = {
+imageSide:'left',
+numberOfImages: 1,
+recordHighlighted: null
+};
+
+
+var demoRecord = [{
+note:'abc def ghi jkl mno pqr stuv wxyz. This is some data, today, yesterday &mdash; now.',
+date:'November 1, 2018',
+image:'dad+benny.jpg',
+base64thumb:''
+}
+];
